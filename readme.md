3c9d039ae72c3cf4a1589c1fc72103f12cf8ddf1
Nate Finch <finchproservices@gmail.com>
2016-04-04 09:29:15 +0900

    middleware

--- readme.md
+++ readme.md
@@ -49,3 +49,52 @@ and then in the `var express`:
 
 makes sure that all the bodies of the forms are parsed before we get to the routes.
 
+
+###Application Setttings###
+
+`app.set()`: used to set a given function.
+`app.enable()` and `app.disable()` are used for boolean functions.
+
+`app.get()`: 
+`app.enabled()` and `app.disabled()` are used for boolean functions.
+
+
+
+```
+app.set('env', 'development'); // process.env.NODE_ENV = 'production'
+app.enable('trust proxy');
+app.set('jsonp callback name', 'cb');
+
+app.set('json replacer', function() {
+	if (attr === 'passwordHash') {
+		return undefined;
+	}
+	return val;
+});
+
+app.get('/user_info', function(req, res) {
+	//get user data
+	res.json(user); //JSON.stringify
+});
+
+app.enable('case sensitive routing'); // /hello and /Hello are different then.
+app.enable('strict routing'); //  /hello/ and  /hello are different.
+
+app.enable('view cache');  //speed up cache
+
+app.set('view engine', 'jade');
+app.set('views', templates);
+
+app.enable('x-powered-by'); // express is the webserver
+```
+
+##Middleware and Express 4##
+
+`app.use()` is what you use to call middleware (e.g. `bodyParser`).
+
+See: 
+https://github.com/senchalabs/connect
+and
+https://github.com/senchalabs/connect/wiki
+
+
--- server.js
+++ server.js
@@ -1,29 +1,62 @@
 var express = require('express'),
-	bodyParser = require('body-parser'),
+	// bodyParser = require('body-parser'),
 	app		= express();
 
 
-app.use(bodyParser.urlencoded());
+// app.set('env', 'development'); // process.env.NODE_ENV = 'production'
+// app.enable('trust proxy');
+// app.set('jsonp callback name', 'cb');
 
-var names = [];
+// app.set('json replacer', function() {
+// 	if (attr === 'passwordHash') {
+// 		return undefined;
+// 	}
+// 	return val;
+// });
 
-app.all('/', function(req, res, next) {
-	console.log('from ALL');
-	next();
-})
+// app.get('/user_info', function(req, res) {
+// 	//get user data
+// 	res.json(user); //JSON.stringify
+// });
 
-app.get('/', function(req, res, next) {
+// app.enable('case sensitive routing'); // /hello and /Hello are different then.
+// app.enable('strict routing'); //  /hello/ and  /hello are different.
+
+// app.enable('view cache');  //speed up cache
+
+// app.set('view engine', 'jade');
+// app.set('views', templates);
+
+// app.enable('x-powered-by'); // express is the webserver
+
+
+function log (req, res, next) {
 	console.log(names);
 	next();
+}
 
-}, function(req, res) {
-	res.render('index.jade', {names: names});
+// app.use(bodyParser.urlencoded());
+
+// var names = [];
+
+
+
+// app.all('/', function(req, res, next) {
+// 	console.log('from ALL');
+// 	next();
+// })
+
+//custom middleware
+app.use(function (req, res, next) {
+	console.log('this will log on every request');
+	next();
 });
 
-app.post('/', function(req, res) {
-	names.push(req.body.name);
-	res.redirect('/');
+app.get('/route', log , function(req, res) {
+	res.send('this is a route');
 });
+
+app.use(express.static('./public'));
 
 app.listen(3000, function() {
 	console.log('listing on port 3000');
